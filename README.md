6d044e370e12e3ea37c674c3ff67c49356c8b7b2
Ivar Soares Urdalen <ivar.urdalen@gmail.com>
2022-11-11 15:27:31 +0100

    Revised CV

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ivar Soares Urdalen's CV
+
+I used R, Quarto to build this CV.
+
+Thanks a lot to [@Nick Strayer](http://nickstrayer.me) and [@beatrizmilz](https://beamilz.com/) for inspiration!
---
+++ export_cv.R
@@ -0,0 +1,4 @@
+pagedown::chrome_print("docs/cv_ivar.html")
+# pdftools::pdf_subset(input = "docs/cv_ivar.pdf", pages =  c(1,2))
+# fs::file_copy(path = "docs/cv_ivar_output.pdf", new_path = "docs/cv_ivar.pdf", overwrite = TRUE)
+
