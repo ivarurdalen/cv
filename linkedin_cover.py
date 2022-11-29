1d388c408e83b1cb69eb21e50d1423b15bcc62e0
Ivar Soares Urdalen <ivar.urdalen@gmail.com>
2022-11-29 10:43:43 +0100

    Updated CV

---
+++ linkedin_cover.py
@@ -0,0 +1,8 @@
+                        # Compact CV for Ivar Soares Urdalen
+                        ivar = {}
+                        ivar["languages"] = ["python", "javascript", "R", "SQL", "Portuguese"]
+                        ivar["frameworks"] = ["fastapi", "dash", "django", "flask"]
+                        ivar["database"] = ["PostgreSQL", "MySQL"]
+                        ivar["cloud_computing"] = ["AWS", "Azure"]
+                        ivar["yrs_work_experience"] = 8
+                        ivar["education"] = ["Studying for MSc in Financial Engineering", "MSc in Civil Engineering"]
